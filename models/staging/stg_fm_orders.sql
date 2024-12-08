d7ea49a22b49c9e6764aeeaa0e4eebb45a496fba
Adarsh Ashok Jha <ajha04@g.syr.edu>
2024-12-08 01:27:14 +0000

    55

--- models/staging/stg_fm_orders.sql
+++ models/staging/stg_fm_orders.sql
@@ -3,8 +3,8 @@
 SELECT
     order_id,
     customer_id,
-    TO_DATE(order_date, 'YYYY-MM-DD') AS order_date,
-    TO_DATE(shipped_date, 'YYYY-MM-DD') AS shipped_date,
+    TRY_TO_DATE(TO_TIMESTAMP(order_date / 1000)) AS order_date, -- Converts timestamp to date
+    TRY_TO_DATE(TO_TIMESTAMP(shipped_date / 1000)) AS shipped_date, -- Converts timestamp to date
     ship_via,
     creditcard_id
 FROM {{ source('fudgemart', 'fm_orders') }}
