81ec5d36b363b345f543faf6110417601872c040
Adarsh Ashok Jha <ajha04@g.syr.edu>
2024-12-08 01:44:59 +0000

    4

--- models/staging/stg_fm_orders.sql
+++ models/staging/stg_fm_orders.sql
@@ -6,12 +6,12 @@ SELECT
     -- Convert microsecond timestamps to DATE
     CASE
         WHEN order_date >= 0 THEN
-            TRY_TO_DATE(TO_TIMESTAMP_NTZ(CAST(order_date / 1000000 AS BIGINT)))
+            CAST(TO_TIMESTAMP_NTZ(CAST(order_date / 1000000 AS BIGINT)) AS DATE)
         ELSE NULL -- Replace invalid or negative values with NULL
     END AS order_date,
     CASE
         WHEN shipped_date >= 0 THEN
-            TRY_TO_DATE(TO_TIMESTAMP_NTZ(CAST(shipped_date / 1000000 AS BIGINT)))
+            CAST(TO_TIMESTAMP_NTZ(CAST(shipped_date / 1000000 AS BIGINT)) AS DATE)
         ELSE NULL
     END AS shipped_date,
     ship_via,
