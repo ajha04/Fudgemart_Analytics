0d7e56546812f13741c080015056b33a7be3300a
Adarsh Ashok Jha <ajha04@g.syr.edu>
2024-12-08 01:20:31 +0000

    4

---
+++ models/marts/dim_customers.sql
@@ -0,0 +1,13 @@
+{{ config(materialized='table') }}
+
+SELECT DISTINCT
+    customer_id,
+    LOWER(customer_firstname) AS customer_firstname,
+    LOWER(customer_lastname) AS customer_lastname,
+    customer_email,
+    customer_address,
+    customer_city,
+    customer_state,
+    customer_zip,
+    customer_phone
+FROM {{ ref('stg_fm_customers') }}
