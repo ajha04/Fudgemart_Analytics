acd579018fb35d6df63397e997c51fe3266aaad8
Adarsh Ashok Jha <ajha04@g.syr.edu>
2024-12-08 01:14:20 +0000

    111

---
+++ models/staging/stg_fm_orders.sql
@@ -0,0 +1,10 @@
+{{ config(materialized='view') }}
+
+SELECT
+    order_id,
+    customer_id,
+    TO_DATE(order_date, 'YYYY-MM-DD') AS order_date,
+    TO_DATE(shipped_date, 'YYYY-MM-DD') AS shipped_date,
+    ship_via,
+    creditcard_id
+FROM {{ source('fudgemart', 'fm_orders') }};
