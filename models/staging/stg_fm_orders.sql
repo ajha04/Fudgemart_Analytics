37bc02cdf71e42f3a46962830c3975ce0799b384
Adarsh Ashok Jha <ajha04@g.syr.edu>
2024-12-09 04:10:28 +0000

    ewlkdew

--- models/staging/stg_fm_orders.sql
+++ models/staging/stg_fm_orders.sql
@@ -3,15 +3,15 @@
 SELECT
     order_id,
     customer_id,
-    -- Convert microsecond timestamps to DATE
+    -- Safely handle microsecond timestamps to convert them to DATE
     CASE
-        WHEN TRY_TO_TIMESTAMP_NTZ(CAST(order_date AS BIGINT) / 1000000) IS NOT NULL THEN
-            CAST(TO_TIMESTAMP_NTZ(CAST(order_date AS BIGINT) / 1000000) AS DATE)
+        WHEN TRY_TO_TIMESTAMP_NTZ(CAST(ROUND(order_date) AS BIGINT) / 1000000) IS NOT NULL THEN
+            CAST(TO_TIMESTAMP_NTZ(CAST(ROUND(order_date) AS BIGINT) / 1000000) AS DATE)
         ELSE NULL
     END AS order_date,
     CASE
-        WHEN TRY_TO_TIMESTAMP_NTZ(CAST(shipped_date AS BIGINT) / 1000000) IS NOT NULL THEN
-            CAST(TO_TIMESTAMP_NTZ(CAST(shipped_date AS BIGINT) / 1000000) AS DATE)
+        WHEN TRY_TO_TIMESTAMP_NTZ(CAST(ROUND(shipped_date) AS BIGINT) / 1000000) IS NOT NULL THEN
+            CAST(TO_TIMESTAMP_NTZ(CAST(ROUND(shipped_date) AS BIGINT) / 1000000) AS DATE)
         ELSE NULL
     END AS shipped_date,
     ship_via,
